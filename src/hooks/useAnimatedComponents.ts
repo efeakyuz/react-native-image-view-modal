2fe187fce31a6152c4996c925de59785b824e903
Efe Akyuz <efenakyuz@gmail.com>
2023-05-05 16:52:44 +0300

    feat: add module hooks

---
+++ src/hooks/useAnimatedComponents.ts
@@ -0,0 +1,42 @@
+import { useEffect } from 'react';
+import { Animated } from 'react-native';
+
+const INITIAL_POSITION = { x: 0, y: 0 };
+const ANIMATION_CONFIG = {
+  duration: 200,
+  useNativeDriver: true,
+};
+
+const useAnimatedComponents = () => {
+  let headerTranslate = new Animated.ValueXY(INITIAL_POSITION);
+
+  const footerTranslate = new Animated.ValueXY(INITIAL_POSITION);
+
+  const toggleVisible = (isVisible: boolean) => {
+    if (isVisible) {
+      Animated.parallel([
+        Animated.timing(headerTranslate.y, { ...ANIMATION_CONFIG, toValue: 0 }),
+        Animated.timing(footerTranslate.y, { ...ANIMATION_CONFIG, toValue: 0 }),
+      ]).start();
+    } else {
+      Animated.parallel([
+        Animated.timing(headerTranslate.y, {
+          ...ANIMATION_CONFIG,
+          toValue: -300,
+        }),
+        Animated.timing(footerTranslate.y, {
+          ...ANIMATION_CONFIG,
+          toValue: 300,
+        }),
+      ]).start();
+    }
+  };
+
+  let headerTransform = headerTranslate.getTranslateTransform();
+
+  const footerTransform = footerTranslate.getTranslateTransform();
+
+  return [headerTransform, footerTransform, toggleVisible] as const;
+};
+
+export default useAnimatedComponents;
